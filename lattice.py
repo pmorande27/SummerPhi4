b432df52904fb80d7fceca72a9bc5defa080b712
pmorande27 <pmorande27@gmail.com>
2024-08-05 16:18:07 +0200

    lint

--- lattice.py
+++ lattice.py
@@ -4,9 +4,9 @@ from alive_progress import alive_bar
 class Lattice(object):
 
 
-    def __init__(self, N, lambda_, N_measurements, N_thermalization, width, HMC= False, epsilon=0, N_steps=0,dim =4):
+    def __init__(self, N, lambda_, N_measurements, N_thermalization, width, HMC = False, epsilon = 0, N_steps = 0, dim = 4):
         """
-        Initialize the lattice with the given parameters.
+        Initializes the lattice with the given parameters.
 
         Parameters:
         N: int, number of sites in each dimension
@@ -28,9 +28,8 @@ class Lattice(object):
         self.shape = [N for i in range(dim)] # shape of the lattice in numpy (library) format
         self.lattice = np.zeros(shape = self.shape) # initialize the lattice
         self.dhs = np.zeros(N_measurements) # array to store the exponential of the change in Hamiltonian for each measurement (test purposes)
-
-
         self.HMCs = HMC
+
         if HMC: # if HMC is True, initialize the HMC parameters
             self.epsilon = epsilon
             self.N_steps = int(N_steps)
@@ -44,37 +43,36 @@ class Lattice(object):
         if self.N_thermalization > 0:
             self.thermalize()
 
-   
     def action(self):
         """
-        Compute the action of the lattice.
+        Computes the action of the lattice.
         """
-        return 1/(2*np.abs(self.lambda_))*np.sum(self.I(self.lattice)**2 )
+        return 1/(2*np.abs(self.lambda_))*np.sum(self.I(self.lattice)**2)
     
-    def metropolis(self,i,j,k,l):
+    def metropolis(self, i, j, k, l):
         """
-        Perform a metropolis update at site (i,j,k,l).
+        Performs a metropolis update at site (i,j,k,l).
         """
         x = i
         y = j
         z = k
         t = l
 
-        old = self.lattice[x,y,z,t]
+        old = self.lattice[x, y, z, t]
         old_action = self.action() #store old action
-        self.lattice[x,y,z,t] += np.random.normal(0,self.width) # propose a new configuration
-        delta = self.action()-old_action # compute the change in action
+        self.lattice[x, y, z, t] += np.random.normal(0, self.width) # propose a new configuration
+        delta = self.action() - old_action # compute the change in action
         
         # accept or reject the new configuration
         if np.random.rand() > np.exp(-delta):
-            self.lattice[x,y,z,t] = old
+            self.lattice[x, y, z, t] = old
         else:
             self.accepted += 1
       
    
     def sweep(self):
         """
-        Perform a sweep of the lattice using the metropolis algorithm, inefficent due to the loop over all sites.
+        Performs a sweep of the lattice using the metropolis algorithm, inefficent due to the loop over all sites.
         """
         for i in range(self.N):
 
@@ -84,10 +82,10 @@ class Lattice(object):
 
                     for l in range(self.N):
 
-                        self.metropolis(i,j,k,l)
+                        self.metropolis(i, j, k, l)
     def thermalize(self):
         """
-        Thermalize the lattice.
+        Thermalizes the lattice.
         It uses the HMC algorithm if HMC is True, otherwise it uses the metropolis algorithm.
         """
         print("Thermalizing")
@@ -106,9 +104,9 @@ class Lattice(object):
         
         print("Thermalization Complete----------------")
       
-    def generate_measurements(self,observable):
+    def generate_measurements(self, observable):
         """
-        Generate the measurements of the observable.
+        Generates the measurements of the observable.
         It uses the HMC algorithm if HMC is True, otherwise it uses the metropolis
         algorithm.
         attribute:
@@ -130,21 +128,21 @@ class Lattice(object):
                 results[i] = observable(self.lattice) # store the result of the observable
                 
                 bar() # this line updates the progress bar, not important
-        print("Measurements Complete----------------",np.average(self.dhs)) # print the average of the exponential of the change in Hamiltonian, should be close to 1
+        print("Measurements Complete----------------", np.average(self.dhs)) # print the average of the exponential of the change in Hamiltonian, should be close to 1
         
         return results
     
     
     def randomize(self):
         """
-        Randomize the lattice.
+        Randomizes the lattice.
         """
         
-        self.lattice = np.random.normal(size=self.shape)
+        self.lattice = np.random.normal(size = self.shape)
     
     def calibration_runs(self,calibration_runs, thermal_runs):
         
-        """ Perform calibration runs to determine the acceptance rate of the HMC algorithm.
+        """ Performs calibration runs to determine the acceptance rate of the HMC algorithm.
         Used to tune the algorithms parameters
         """
 
@@ -170,7 +168,7 @@ class Lattice(object):
         else:
             return self.accepted/(calibration_runs*self.N**4)
 
-    def measure_difference(dim,lattice):
+    def measure_difference(dim, lattice):
         """
         Measures (phi(x)-phi(y))^2
         """
@@ -204,53 +202,73 @@ class Lattice(object):
 
         for i in range(int(N/2)):
             for j in range(dim):
-                shift = np.roll(lattice, i+1, axis=j)
-                result[i, j] =( lattice - shift)**2
+                shift = np.roll(lattice, i+1, axis = j)
+                result[i, j] =(lattice - shift)**2
         #print(result)
         #print(result)
-        axis = tuple([i for i in range(2,dim+2)])
-        final = np.sum(result, axis=axis) / (dim * N**dim)
+        axis = tuple([i for i in range(2, dim+2)])
+        final = np.sum(result, axis = axis) / (dim * N**dim)
         return final
-    def measure_field(dim,lattice):
+    def measure_field(dim, lattice):
         """
         Measures phi(x)
         """
         return np.sum(lattice)/(len(lattice)**dim)
     
-    def backwards(self,lattice,axis):
+    def backwards(self, lattice,axis):
         """
         Computes backward coefficient for the molecular dynamics evolution.
         """
-        result = (1+2*lattice-2*np.roll(lattice,1,axis))
+        result = (1 + 2*lattice - 2*np.roll(lattice, 1, axis)) #np.roll(lattice, 1, axis) is phi(x-mu) in the notes, axis plays the role of mu in the notes
         return result
     
-    def forwards(self,lattice,axis):
+    def forwards(self, lattice, axis):
         """
         Computes forward coefficient for the molecular dynamics evolution.
         """
         result = 1
         return result
-    def centre(self,lattice,axis):
+    
+    def centre(self, lattice, axis):
         """
         Computes centre coefficient for the molecular dynamics evolution.
         """
-        return 2*lattice-2-2*np.roll(lattice,-1,axis)
-    def molecular_dynamics(self, N_steps, epsilon,p_0,phi_0):
+        return 2*lattice - 2 - 2*np.roll(lattice, -1, axis) #np.roll(lattice, -1, axis) is phi(x+mu) in the notes, axis plays the role of mu in the notes
+    
+    def molecular_dynamics(self, N_steps, epsilon, p_0, phi_0):
+        """
+        
+        Performs the molecular dynamics evolution of the Hamiltonian Monte Carlo algorithm.
+        
+        Parameters:
+        N_steps: int, number of steps in the molecular dynamics evolution
+        epsilon: float, step size for the molecular dynamics evolution
+        p_0: numpy array, the momentum at the beginning of the evolution
+        phi_0: numpy array, the lattice at the beginning of the evolution
+
+        Returns:
+        numpy array, the momentum at the end of the evolution
+        numpy array, the lattice at the end of the evolution
+        """
 
         
-        p = p_0 + epsilon/2*self.dot_p(phi_0)
-        phi = phi_0.copy()
+        p = p_0 + epsilon/2*self.dot_p(phi_0) # half step for the momentum
+        phi = phi_0.copy() # copy the lattice
+        
         for i in range(N_steps):
-            phi +=  epsilon*p
+            phi +=  epsilon*p # full step for the lattice
+            
             if i == N_steps-1:
-                p +=  epsilon/2*self.dot_p(phi)
+                p +=  epsilon/2*self.dot_p(phi) # half step for the momentum in the last step
+            
             else:
-                p += epsilon*self.dot_p(phi)
+                p += epsilon*self.dot_p(phi) # full step for the momentum in the other steps
+        
         return p,phi        
     
     def HMC(self, N_steps, epsilon,flag = False):
         """
-        Perform a Hamiltonian Monte Carlo update.
+        Performs a Hamiltonian Monte Carlo update.
 
         Parameters:
         N_steps: int, number of steps in the molecular dynamics evolution
@@ -275,7 +293,7 @@ class Lattice(object):
 
 
         return delta_H
-    def I(self,lattice):
+    def I(self, lattice):
         """
         Calculates the factor I(x) for the action and the dot_p function.
 
@@ -287,12 +305,12 @@ class Lattice(object):
         """
         result = 0
         for i in range(self.dim):
-            forward = np.roll(lattice,-1,axis=i)
-            backward = np.roll(lattice,1,axis=i)
+            forward = np.roll(lattice, -1, axis = i)
+            backward = np.roll(lattice, 1, axis = i)
             result += forward + backward - 2*lattice + (forward-lattice)**2
         return result 
      
-    def actions(self,lattice):
+    def actions(self, lattice):
         """
         Compute the action of the lattice
 
@@ -306,9 +324,9 @@ class Lattice(object):
     
     
     
-    def dot_p(self,lattice):
+    def dot_p(self, lattice):
         """
-        Compute the evolution of the momentum in the Hamiltonian Monte Carlo algorithm. dot p = -dS/dphi
+        Computes the evolution of the momentum in the Hamiltonian Monte Carlo algorithm. dot p = -dS/dphi
 
         Parameters:
         lattice: numpy array, the lattice
@@ -320,9 +338,9 @@ class Lattice(object):
         I = self.I(lattice)
         result = 0
         for i in range(self.dim):
-            Foward = np.roll(I,-1,axis=i)
-            Backward = np.roll(I,1,axis=i)*(1+2*lattice-2*np.roll(lattice,1,i))
-            Centre = I*(2*lattice-2-2*np.roll(lattice,-1,i))
+            Foward = np.roll(I,-1,axis = i)
+            Backward = np.roll(I,1,axis = i)*(1 + 2*lattice - 2*np.roll(lattice, 1, i))
+            Centre = I*(2*lattice - 2 - 2*np.roll(lattice, -1, i))
             result += Foward + Backward + Centre
         return -1/np.abs(self.lambda_)*result # return the evolution of the momentum, don't forget the minus sign
            
